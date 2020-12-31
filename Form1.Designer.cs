9c7fa7493cebc31114dba962557ada85acdd2c12
OstapchukAB <electron.anton@gmail.com>
2020-12-31 21:37:18 +0300

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxLine = new System.Windows.Forms.CheckBox();
+            this.checkBoxPoint = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBoxMeasurementMode = new System.Windows.Forms.CheckBox();
+            this.checkBoxZoom = new System.Windows.Forms.CheckBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1290, 703);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // checkBoxLine
+            // 
+            this.checkBoxLine.AutoSize = true;
+            this.checkBoxLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxLine.Location = new System.Drawing.Point(640, 37);
+            this.checkBoxLine.Name = "checkBoxLine";
+            this.checkBoxLine.Size = new System.Drawing.Size(58, 24);
+            this.checkBoxLine.TabIndex = 2;
+            this.checkBoxLine.Text = "Line";
+            this.checkBoxLine.UseVisualStyleBackColor = true;
+            this.checkBoxLine.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // checkBoxPoint
+            // 
+            this.checkBoxPoint.AutoSize = true;
+            this.checkBoxPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxPoint.Location = new System.Drawing.Point(716, 37);
+            this.checkBoxPoint.Name = "checkBoxPoint";
+            this.checkBoxPoint.Size = new System.Drawing.Size(64, 24);
+            this.checkBoxPoint.TabIndex = 3;
+            this.checkBoxPoint.Text = "Point";
+            this.checkBoxPoint.UseVisualStyleBackColor = true;
+            this.checkBoxPoint.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.checkBoxMeasurementMode);
+            this.groupBox2.Controls.Add(this.checkBoxZoom);
+            this.groupBox2.Controls.Add(this.checkBoxPoint);
+            this.groupBox2.Controls.Add(this.checkBoxLine);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(182, 721);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1063, 82);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // checkBoxMeasurementMode
+            // 
+            this.checkBoxMeasurementMode.AutoSize = true;
+            this.checkBoxMeasurementMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxMeasurementMode.Location = new System.Drawing.Point(357, 37);
+            this.checkBoxMeasurementMode.Name = "checkBoxMeasurementMode";
+            this.checkBoxMeasurementMode.Size = new System.Drawing.Size(164, 24);
+            this.checkBoxMeasurementMode.TabIndex = 5;
+            this.checkBoxMeasurementMode.Text = "Режим измерений";
+            this.checkBoxMeasurementMode.UseVisualStyleBackColor = true;
+            this.checkBoxMeasurementMode.CheckedChanged += new System.EventHandler(this.checkBoxMeasurementMode_CheckedChanged);
+            // 
+            // checkBoxZoom
+            // 
+            this.checkBoxZoom.AutoSize = true;
+            this.checkBoxZoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxZoom.Location = new System.Drawing.Point(551, 37);
+            this.checkBoxZoom.Name = "checkBoxZoom";
+            this.checkBoxZoom.Size = new System.Drawing.Size(69, 24);
+            this.checkBoxZoom.TabIndex = 4;
+            this.checkBoxZoom.Text = "Zoom";
+            this.checkBoxZoom.UseVisualStyleBackColor = true;
+            this.checkBoxZoom.CheckedChanged += new System.EventHandler(this.Zoom_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 816);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxLine;
+        private System.Windows.Forms.CheckBox checkBoxPoint;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxZoom;
+        private System.Windows.Forms.CheckBox checkBoxMeasurementMode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        Chart MyChart;
+        //List<A> LstA = new List<A>();
+        SortedSet<A> LstA = new SortedSet<A>(new ACompare());
+        string[] NameSeries = {"Line","Point" };
+        struct A {
+            public int id { get; set; }
+            public string v { get; set; }
+
+            public DateTime dtm { get; set; }
+
+            public A(int id, string v, DateTime dtm)
+            {
+                this.id = id;
+                this.v = v;
+                this.dtm = dtm;
+            }
+        }
+        //Реализация сортировки структуры A
+        class ACompare : Comparer<A>
+        {
+            public override int Compare(A x, A y)
+            {
+                if (object.Equals(x, y)) return 0;
+                return x.dtm.CompareTo(y.dtm); 
+            }
+        }
+
+        Point? prevPosition = null;
+        ToolTip tooltip = new ToolTip();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LstA.Clear();
+            MyChart.Series.Clear();  
+            DateTime dtm = DateTime.Now;
+            var rnd = new Random();
+            for (int i = 0; i < 1000; i++) {
+                var v = i.ToString("D2");
+               
+                var a = new A( rnd.Next(1,100), v, dtm.AddDays(rnd.Next(0, 1000)));
+                LstA.Add(a);
+
+            }
+            //LstA.Sort(new ACompare());   
+
+
+                   
+
+            Series s = new Series(NameSeries[0]);
+            s.Legend = "myLegend";
+            s.ChartType = SeriesChartType.Line;
+            s.Color = Color.Blue;
+
+            Series sPoint = new Series(NameSeries[1]);
+            sPoint.Legend = "myLegend";
+            sPoint.ChartType = SeriesChartType.Point;
+            sPoint.Color = Color.DarkBlue;    
+            foreach (A a in LstA) 
+            {
+                s.Points.AddXY(a.dtm, a.id);
+                sPoint.Points.AddXY(a.dtm, a.id);
+            }
+            MyChart.Series.Add(s);
+            MyChart.Series.Add(sPoint);
+            checkBoxLine.Checked = true;
+            checkBoxPoint.Checked = true;  
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MyChart = new Chart();
+            MyChart.Parent = groupBox1;
+            MyChart.Dock = DockStyle.Fill;
+            MyChart.DataSource = LstA;
+            MyChart.ChartAreas.Add(new ChartArea("myChartArea"));
+            MyChart.Legends.Add(new Legend("myLegend"));
+            MyChart.ChartAreas[0].AxisY.Title = "Коэффициент";
+            MyChart.ChartAreas[0].AxisX.Title = "Дата";
+            
+           
+
+            checkBoxLine.Text = NameSeries[0];
+            checkBoxPoint.Text = NameSeries[1];
+            
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox ch = (CheckBox)sender;
+            Series s = MySeriesFind(ch.Text);
+            if (s !=null)
+            if (ch.Checked)
+                s.Enabled = true;
+            else
+                s.Enabled = false;
+           
+        }
+
+        private Series MySeriesFind(string nameSeries) {
+            return MyChart.Series.FindByName(nameSeries);
+           
+        }
+
+        private void Zoom_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxZoom.Checked)
+            {
+                MyChart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+                MyChart.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+                MyChart.MouseWheel += MyChart_MouseWheel;
+            }
+            else {
+               
+                var xAxis = MyChart.ChartAreas[0].AxisX;
+                var yAxis = MyChart.ChartAreas[0].AxisY;
+                xAxis.ScaleView.ZoomReset();
+                yAxis.ScaleView.ZoomReset();
+                MyChart.ChartAreas[0].AxisX.ScaleView.Zoomable = false;
+                MyChart.ChartAreas[0].AxisY.ScaleView.Zoomable = false;
+                MyChart.MouseWheel -= MyChart_MouseWheel;
+            }
+        }
+
+        private void MyChart_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var chart = (Chart)sender;
+            var xAxis = chart.ChartAreas[0].AxisX;
+            var yAxis = chart.ChartAreas[0].AxisY;
+
+            try
+            {
+                if (e.Delta < 0) // Scrolled down.
+                {
+                    xAxis.ScaleView.ZoomReset();
+                    yAxis.ScaleView.ZoomReset();
+                }
+                else if (e.Delta > 0) // Scrolled up.
+                {
+                    var xMin = xAxis.ScaleView.ViewMinimum;
+                    var xMax = xAxis.ScaleView.ViewMaximum;
+                    var yMin = yAxis.ScaleView.ViewMinimum;
+                    var yMax = yAxis.ScaleView.ViewMaximum;
+                    double k =2;
+
+                    var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / k;
+                    var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / k;
+                    var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / k;
+                    var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / k;
+
+                    xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                    yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+
+
+
+        private void checkBoxMeasurementMode_CheckedChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("checkBoxMeasurementMode_CheckedChanged");
+           
+
+            if (checkBoxMeasurementMode.Checked)
+            {
+                MyChart.Cursor = Cursors.Cross;
+                //labelPosCursor.BackColor = Color.White;
+                MyChart.MouseMove += new System.Windows.Forms.MouseEventHandler(myChart_MouseMove);
+            }
+            else
+            {
+                MyChart.Cursor = Cursors.Default;
+               // labelPosCursor.BackColor = default(Color);
+                //labelPosCursor.Text = "";
+                tooltip.RemoveAll();
+                MyChart.MouseMove -= new System.Windows.Forms.MouseEventHandler(myChart_MouseMove);
+            }
+        }
+
+
+        async private void myChart_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            //labelPosCursor.BackColor = Color.White;
+            var pos = e.Location;
+            if (prevPosition.HasValue && pos == prevPosition.Value)
+                return;
+            tooltip.RemoveAll();
+            prevPosition = pos;
+            var pX = pos.X;
+            var pY = pos.Y;
+            var show = "";
+
+            await Task.Run(() =>
+            {
+                Action action = () =>
+                {
+
+
+
+                    var results = MyChart.HitTest(pX, pY, false, ChartElementType.DataPoint); // 
+                    foreach (var result in results)
+                    {
+                        if (result.ChartElementType == ChartElementType.DataPoint) // 
+                        {
+                            var yVal = result.ChartArea.AxisY.PixelPositionToValue(pY);
+                            var xVal = DateTime.FromOADate(result.ChartArea.AxisX.PixelPositionToValue(pX));
+                            show = $"{xVal}, {(int)yVal}";
+                            //labelPosCursor.Text = show;
+
+                            //tooltip.Show(show, myChart, pX, pY - 20);
+                        }
+                    }
+                };
+                if (InvokeRequired)
+                    BeginInvoke(action);
+                else
+                    action();
+            });
+
+            //labelPosCursor.Text = show;
+            tooltip.Show(show, MyChart, pX, pY - 20);
+        }
+
+      
+    }
+}
